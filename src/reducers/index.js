10fd06902f135c995ac8589c4b7dda6f3d2416ab
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2018-01-31 13:58:27 +0200

    redux begins

---
+++ src/reducers/index.js
@@ -0,0 +1,21 @@
+import { ADD_REMINDER } from '../constants'
+
+const reminder = (action) => {
+  return {
+    text: action.text,
+    id: Math.random()
+  }
+}
+const reminders = (state = [], action) => {
+  let reminders = null
+  switch(action.type) {
+    case ADD_REMINDER:
+      reminders = [...state, reminder(action)]
+      console.log("reminders", reminders)
+      return reminders
+    default: 
+    return state
+  }
+}
+
+export default reminders
