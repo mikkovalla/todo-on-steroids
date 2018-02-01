f28356ca3faf9055959da5222a997fb45330ba71
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2018-02-01 23:45:16 +0200

    discovering Moment.js library... life just became a little easier....

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { addReminder, deleteReminder } from '../actions'
+import moment from 'moment'
 
 
 class App extends Component {
@@ -32,10 +33,18 @@ class App extends Component {
       <ul className='list-group col-sm-4'>
         {
           reminders.map( reminder => 
-          <li key={reminder.id} className="list-group-item">
-            <div className="list-item">{reminder.text}</div>
-            <div className="list-item delete-button"
-            onClick={() => this.deleteReminder(reminder.id)}>
+          <li 
+          key={reminder.id} 
+          className="list-group-item"
+          >
+            <div className="list-item">
+              <div>{reminder.text}</div>
+              <div><em>{moment(new Date(reminder.dueDate)).fromNow()}</em></div>
+            </div>
+            <div 
+            className="list-item delete-button"
+            onClick={() => this.deleteReminder(reminder.id)}
+            >
             &#x2715;
             </div>
           </li>
