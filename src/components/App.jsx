10e68abb0ea469c35fee219f00b1a65a233f7352
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2018-02-01 23:36:09 +0200

    wohoo, date functionality added with redux, so happy :D

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,13 +8,15 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      text: ' '
+      text: ' ',
+      dueDate: ''
     }
   }
 
   addReminder() {
     //console.log('state:', this)
-    this.props.addReminder(this.state.text)
+    console.log("due date: ", this.state.dueDate)
+    this.props.addReminder(this.state.text, this.state.dueDate)
   }
 
   deleteReminder (id) {
@@ -52,6 +54,7 @@ class App extends Component {
         <div className="form-inline reminder-form">
           <div className="form-group">
             <input className="form-control" placeholder="I have to..." onChange={event => this.setState({text: event.target.value})}/>
+            <input className="form-control" type="datetime-local" onChange={event => this.setState({dueDate: event.target.value})}/>
           </div>
           <button type="button" className="btn btn-success" onClick={() => this.addReminder()}>
           New todo
